59cd259e623620fc297c2d11d27a081ca029af45
Saad Aulakh <saadaulakh04@gmail.com>
2022-10-04 21:15:24 -0400

    Deploy React App to GitHub

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import PrintProvider, { NoPrint, Print } from 'react-easy-print';
+import uniqid from 'uniqid';
+import EducationSection from './components/education';
+import ExpirienceSection from './components/experience';
+import GeneralSection from './components/general';
+import './styles/App.css';
+import './styles/forms.css';
 
-function App() {
+const App = () => {
+  const [educationIds, setEducationIds] = useState([]);
+  const [experienceIds, setExperienceIds] = useState([]);
+
+  const handleClick = (type) => {
+    if (type === 'experienceIds') {
+      setExperienceIds((prevExpIds) => [...prevExpIds, uniqid()]);
+    } else {
+      setEducationIds((prevEduIds) => [...prevEduIds, uniqid()]);
+    }
+  };
+
+  const handleDelete = (type, id) => {
+    if (type === 'experienceIds') {
+      setExperienceIds((prevExpIds) => {
+        let newList = prevExpIds.filter((key) => key !== id);
+        return newList;
+      });
+    } else {
+      setEducationIds((prevEduIds) => {
+        let newList = prevEduIds.filter((key) => key !== id);
+        return newList;
+      });
+    }
+  };
+
+  const eduComponents = educationIds.map((id) => (
+    <EducationSection key={id} id={id} handleDelete={handleDelete} />
+  ));
+  const expComponents = experienceIds.map((id) => (
+    <ExpirienceSection key={id} id={id} handleDelete={handleDelete} />
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PrintProvider>
+        <NoPrint>
+          <main>
+            <h1 className='title'>CV-Application Creator</h1>
+            <Print>
+              <h2 className='subTitle'>General Information</h2>
+              <GeneralSection />
+            </Print>
+            <div>
+              <Print>
+                <h2 className='subTitle'>Education</h2>
+                {eduComponents}
+              </Print>
+              <button
+                className='addBtn'
+                onClick={() => handleClick('educationIds')}>
+                Add
+              </button>
+            </div>
+            <div>
+              <Print>
+                <h2 className='subTitle'>Work Experience</h2>
+                {expComponents}
+              </Print>
+              <button
+                className='addBtn'
+                onClick={() => handleClick('experienceIds')}>
+                Add
+              </button>
+            </div>
+            <button
+              onClick={() => {
+                window.print();
+              }}
+              className='addBtn'>
+              Print
+            </button>
+          </main>
+        </NoPrint>
+      </PrintProvider>
     </div>
   );
-}
+};
 
 export default App;
